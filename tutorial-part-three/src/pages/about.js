df963ab3f10c535cc63070f2440be589b8209478
Christian Hughes <chughes.mail@icloud.com>
2020-09-08 16:52:16 -0500

    Complete tutorial parts 2 & 3.

---
+++ tutorial-part-three/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <h1>About me</h1>
+      <p>
+        I’m good enough, I’m smart enough, and gosh darn it, people like me!
+      </p>
+    </Layout>
+  )
+}
---
+++ tutorial-part-three/src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Contact() {
+  return (
+    <Layout>
+      <h1>I'd love to talk! Email me at the address below</h1>
+      <p>
+        <a href="mailto:me@example.com">me@example.com</a>
+      </p>
+    </Layout>
+  )
+}
