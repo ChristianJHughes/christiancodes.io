d41869201bfd1e650e6f11f8809685ca8fd6c450
Christian Hughes <christian.hughes@daveramsey.com>
2021-07-06 23:58:48 -0500

    Add Tailwind. Configure Dark theme support. (#2)
    * Add Tailwind. Configure Dark theme support.
    
    * remove empty dir

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  modeL: "jit",
+  purge: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
