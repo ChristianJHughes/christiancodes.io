e03a93ad87faa25e97e6a08bff1375aeb38163c5
Christian Hughes <christian.hughes@hey.com>
2021-07-12 21:19:50 -0500

    More Headwind.

--- components/Layout.js
+++ components/Layout.js
@@ -4,10 +4,10 @@ export default function Layout({ children }) {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center space-y-4">
+    <div className="flex flex-col items-center justify-center h-screen space-y-4 text-center">
       {children}{" "}
       <button
-        className="p-3 bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-800 rounded-md"
+        className="p-3 bg-gray-200 rounded-md hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-800"
         onClick={() => setTheme(theme == "dark" ? "light" : "dark")}
       >
         Toggle Dark Mode
