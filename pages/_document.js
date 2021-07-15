f2e153a68e1a6572424b95921fd6ce32348131e0
Christian Hughes <christian.hughes@daveramsey.com>
2021-07-14 21:11:11 -0500

    Add Inter Font. (#6)
    * Add Inter Font.
    
    * Delete comment.
    
    * Follow Tailwind guidance for font.

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {/* https://leerob.io/blog/fonts */}
+          <link
+            rel="preload"
+            href="/fonts/inter-var-latin.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+        </Head>
         <body className="px-4 text-black bg-white dark:text-white dark:bg-black lg:px-0">
           <Main />
           <NextScript />
